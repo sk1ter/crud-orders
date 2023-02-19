9365a8ad06e80e533f0b370a504d562321e7afe3
Javlonbek <javlonbek.sharipoff@gmail.com>
2023-02-19 13:15:07 +0500

    added laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth']);
+    Route::get('/dashboard', [\App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
