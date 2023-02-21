386acaf2b0932d349fd35f4263602204f715375f
Javlonbek <javlonbek.sharipoff@gmail.com>
2023-02-21 19:40:23 +0500

    added price

--- app/Http/Livewire/OrdersTable.php
+++ app/Http/Livewire/OrdersTable.php
@@ -5,7 +5,9 @@ namespace App\Http\Livewire;
 use App\Models\Order;
 use Mediconesystems\LivewireDatatables\Action;
 use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\DateColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Mediconesystems\LivewireDatatables\LabelColumn;
 use Mediconesystems\LivewireDatatables\NumberColumn;
 
 class OrdersTable extends LivewireDatatable
@@ -18,6 +20,7 @@ class OrdersTable extends LivewireDatatable
         return Order::query();
     }
 
+
     public function columns()
     {
         return [
@@ -25,13 +28,30 @@ class OrdersTable extends LivewireDatatable
                 ->label(__('ID'))
                 ->linkTo('orders/update'),
             Column::name('email')
-                ->label(__('E-mail')),
+                ->label(__('E-mail'))
+                ->filterable()
+                ->searchable(),
             Column::name('phone')
-                ->label(__('Phone')),
+                ->label(__('Phone'))
+                ->filterable()
+                ->searchable(),
+            Column::callback('id', function ($id) {
+                $q = \DB::select('select sum((ohp.quantity * p.price) / 100) as sum from order_has_product ohp left join products p on p.id = ohp.product_id where order_id = '. $id .' order by order_id');
+                return $q[0]->sum ?? 0;
+            }),
             Column::name('address')
-                ->label(__('Address')),
-            Column::name('order_date')
+                ->label(__('Address'))
+                ->filterable()
+                ->searchable(),
+            DateColumn::name('order_date')
                 ->label(__('Order date'))
+                ->filterable()
+                ->searchable()
+            ->width(300)
         ];
+    }
+
+    public function calculateAmount($products)
+    {
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <a href="{{route('order.create')}}"
-                    class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                    class="text-gray-900 bg-white block w-40 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-4 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                 {{__('Add order')}}
             </a>
             <livewire:orders-table/>
